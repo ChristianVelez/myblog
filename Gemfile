c9525ed40463b0c064d656e12ee2af3a2ce6b4b6
Christian Velez <christian.velez1@gmail.com>
2016-07-29 14:30:22 -0400

    added admin flag and removed devise admin

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'devise', '~> 4.1', '>= 4.1.1'
 
+gem 'cancancan', '~> 1.15'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
  class PostsController < ApplicationController
 	before_action :authenticate_user!, except: [:index, :show]
-	before_filter :authenticate_admin!, only: [:create, :destroy]
+	
 
 	def index
 		@posts = Post.all.order('created_at DESC')
